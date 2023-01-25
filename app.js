1201db869a63cd778d4535707b136d55f5dc0f43
Ermin26 <jolda.ermin@gmail.com>
2023-01-25 12:02:39 +0100

    Fixed bug for edit buyed products more than 1

--- app.js
+++ app.js
@@ -585,35 +585,37 @@ app.get('/all/:id', isLoged, async (req, res) => {
 app.get('/all/:id/edit', isLoged, async (req, res) => {
     const { id } = req.params
     const user = await User.findById(id);
+
     if (!user) {
         return res.redirect('/all')
     }
     else {
-        return res.render('edit', { user })
-
+        for(let product of user.products){
+            return res.render('edit', { user,product })
+        }
     }
-
 });
 app.put('/all/:id/products', isLoged, async (req, res) => {
     const { id } = req.params;
     const data = req.body;
-    const productId = req.body.productId;
     const user = await User.findById(id);
-
-    for (let i = 0; i < user.products.length; i++) {
-        if (productId === user.products[i].id) {
-            await user.products[i].name.pop()
-            await user.products[i].qty.pop()
-            await user.products[i].price.pop()
-            await user.products[i].total.pop()
-            await user.products[i].name.push(data.productName);
-            await user.products[i].qty.push(data.productQty);
-            await user.products[i].price.push(data.productPrice);
-            await user.products[i].total.push(data.productTotal);
-            await user.save();
+   for(let j = 0; j< data.productId.length; j++){
+    for(let i = 0; i< user.products.length; i++){
+    if(data.productId[j] === user.products[i].id){
+        
+                await user.products[i].name.pop()
+                await user.products[i].qty.pop()
+                await user.products[i].price.pop()
+                await user.products[i].total.pop()
+                await user.products[i].name.push(data.productName[j]);
+                await user.products[i].qty.push(data.productQty[j]);
+                await user.products[i].price.push(data.productPrice[j]);
+                await user.products[i].total.push(data.productTotal[j]);
+                await user.save();
+                
+            }
         }
     }
-
     res.redirect('/all');
 })
 //? DELA
